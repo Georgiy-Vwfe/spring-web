9cb03092e08608a42b9eb9d9853d401520a32763
Georgiy-Vwfe <postnikov.george@yandex.ru>
2019-02-21 10:36:43 +0300

    include bootstrap in all pages

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -27,6 +27,29 @@ public class GreetingController {
         return "greeting";
     }
 
+    @GetMapping("/contacts")
+    public String contacts(@RequestParam(name = "name", required = false, defaultValue = "sample text") String name,
+                           @RequestParam(name = "phone", required = false, defaultValue = "sample text") String phone,
+                           @RequestParam(name = "email", required = false, defaultValue = "sample text") String email,
+                           Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("phone", phone);
+        model.addAttribute("email", email);
+
+        return "contacts";
+    }
+
+    @GetMapping("/about")
+    public String about(@RequestParam(name = "education", required = false, defaultValue = "sample text") String education,
+                        @RequestParam(name = "job", required = false, defaultValue = "sample text") String job,
+                        @RequestParam(name = "projects", required = false, defaultValue = "sample text") String projects,
+                        Model model) {
+        model.addAttribute("education", education);
+        model.addAttribute("job", job);
+        model.addAttribute("projects", projects);
+        return "about";
+    }
+
     @GetMapping("/news")
     public String news(Model model) throws IOException {
         List<News> news = repository.readNews();
